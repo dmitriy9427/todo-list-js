877c9251fedc4a018b8c99b8d36294031f4af8b3
Dima Ryabov <dima.poltergeist.2015@yandex.ru>
2023-08-03 15:04:40 +0300

    Updates

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
-(()=>{"use strict";var n=function(n){return document.querySelector(".".concat(n))},t=n("todo__form-input"),e=n("todo__form-btn"),c=n("todo__list"),o=n("popup-edit__btn-close"),i=(n("popup-edit__btn-save"),n("popup-edit__btn-cancel")),l=(n("popup-edit__input"),n("popup")),s=JSON.parse(localStorage.getItem("arr"))||[];function a(){c.innerHTML="";for(var n=0;n<s.length;n++){var t=document.createElement("li");t.insertAdjacentHTML("afterbegin",'\n    <input id="checbox" class="todo__list-item-input" type="checkbox" />\n    <label for="checbox" class="todo__list-item-label"></label>\n    <span class="todo__list-item-text">'.concat(s[n],'</span>\n    <div class="todo__list-item-svg">\n      \n    <svg\n    onClick=editTodoElement(').concat(n,')\n        width="30px"\n        height="30px"\n        viewBox="0 0 24 24"\n        fill="none"\n        xmlns="http://www.w3.org/2000/svg"\n      >\n        <path\n          fill-rule="evenodd"\n          clip-rule="evenodd"\n          d="M21.1213 2.70705C19.9497 1.53548 18.0503 1.53547 16.8787 2.70705L15.1989 4.38685L7.29289 12.2928C7.16473 12.421 7.07382 12.5816 7.02986 12.7574L6.02986 16.7574C5.94466 17.0982 6.04451 17.4587 6.29289 17.707C6.54127 17.9554 6.90176 18.0553 7.24254 17.9701L11.2425 16.9701C11.4184 16.9261 11.5789 16.8352 11.7071 16.707L19.5556 8.85857L21.2929 7.12126C22.4645 5.94969 22.4645 4.05019 21.2929 2.87862L21.1213 2.70705ZM18.2929 4.12126C18.6834 3.73074 19.3166 3.73074 19.7071 4.12126L19.8787 4.29283C20.2692 4.68336 20.2692 5.31653 19.8787 5.70705L18.8622 6.72357L17.3068 5.10738L18.2929 4.12126ZM15.8923 6.52185L17.4477 8.13804L10.4888 15.097L8.37437 15.6256L8.90296 13.5112L15.8923 6.52185ZM4 7.99994C4 7.44766 4.44772 6.99994 5 6.99994H10C10.5523 6.99994 11 6.55223 11 5.99994C11 5.44766 10.5523 4.99994 10 4.99994H5C3.34315 4.99994 2 6.34309 2 7.99994V18.9999C2 20.6568 3.34315 21.9999 5 21.9999H16C17.6569 21.9999 19 20.6568 19 18.9999V13.9999C19 13.4477 18.5523 12.9999 18 12.9999C17.4477 12.9999 17 13.4477 17 13.9999V18.9999C17 19.5522 16.5523 19.9999 16 19.9999H5C4.44772 19.9999 4 19.5522 4 18.9999V7.99994Z"\n          fill="#000000"\n        />\n      </svg>\n \n      <svg\n      onclick=deleteTodoElement(').concat(n,')\n      width="30px"\n      height="30px"\n      viewBox="0 0 1024 1024"\n      class="icon"\n      version="1.1"\n      xmlns="http://www.w3.org/2000/svg"\n    >\n      <path\n        d="M667.8 362.1H304V830c0 28.2 23 51 51.3 51h312.4c28.4 0 51.4-22.8 51.4-51V362.2h-51.3z"\n        fill="#CCCCCC"\n      />\n      <path\n        d="M750.3 295.2c0-8.9-7.6-16.1-17-16.1H289.9c-9.4 0-17 7.2-17 16.1v50.9c0 8.9 7.6 16.1 17 16.1h443.4c9.4 0 17-7.2 17-16.1v-50.9z"\n        fill="#CCCCCC"\n      />\n      <path\n        d="M733.3 258.3H626.6V196c0-11.5-9.3-20.8-20.8-20.8H419.1c-11.5 0-20.8 9.3-20.8 20.8v62.3H289.9c-20.8 0-37.7 16.5-37.7 36.8V346c0 18.1 13.5 33.1 31.1 36.2V830c0 39.6 32.3 71.8 72.1 71.8h312.4c39.8 0 72.1-32.2 72.1-71.8V382.2c17.7-3.1 31.1-18.1 31.1-36.2v-50.9c0.1-20.2-16.9-36.8-37.7-36.8z m-293.5-41.5h145.3v41.5H439.8v-41.5z m-146.2 83.1H729.5v41.5H293.6v-41.5z m404.8 530.2c0 16.7-13.7 30.3-30.6 30.3H355.4c-16.9 0-30.6-13.6-30.6-30.3V382.9h373.6v447.2z"\n        fill="#211F1E"\n      />\n      <path\n        d="M511.6 798.9c11.5 0 20.8-9.3 20.8-20.8V466.8c0-11.5-9.3-20.8-20.8-20.8s-20.8 9.3-20.8 20.8v311.4c0 11.4 9.3 20.7 20.8 20.7zM407.8 798.9c11.5 0 20.8-9.3 20.8-20.8V466.8c0-11.5-9.3-20.8-20.8-20.8s-20.8 9.3-20.8 20.8v311.4c0.1 11.4 9.4 20.7 20.8 20.7zM615.4 799.6c11.5 0 20.8-9.3 20.8-20.8V467.4c0-11.5-9.3-20.8-20.8-20.8s-20.8 9.3-20.8 20.8v311.4c0 11.5 9.3 20.8 20.8 20.8z"\n        fill="#211F1E"\n      />\n    </svg>\n      \n    </div>')),t.className="todo__list-item",c.prepend(t)}}function p(){var n=t.value;n.length>0&&(s.push(n),t.value=""),localStorage.setItem("arr",JSON.stringify(s)),a()}function d(){l.classList.toggle("open-popup")}e.addEventListener("click",(function(n){n.preventDefault(),p()})),e.removeEventListener("click",(function(n){n.preventDefault(),p()})),i.addEventListener("click",d),o.addEventListener("click",d),a()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/index.css */ \"./src/styles/index.css\");\n\nvar getElementDom = function getElementDom(element) {\n  return document.querySelector(\".\".concat(element));\n};\nvar createItem = function createItem(item) {\n  return document.createElement(item);\n};\nvar todoInput = getElementDom(\"todo__form-input\");\nvar todoAddBtn = getElementDom(\"todo__form-btn\");\nvar todoList = getElementDom(\"todo__list\");\nvar popupCloseBtn = getElementDom(\"popup-edit__btn-close\");\nvar popupSaveBtn = getElementDom(\"popup-edit__btn-save\");\nvar popupCancelBtn = getElementDom(\"popup-edit__btn-cancel\");\nvar popupInput = getElementDom(\"popup-edit__input\");\nvar popup = getElementDom(\"popup\");\nvar localArr = JSON.parse(localStorage.getItem(\"arr\")) || [];\nvar setLocalStorage = function setLocalStorage() {\n  return localStorage.setItem(\"arr\", JSON.stringify(localArr));\n};\nfunction render() {\n  todoList.innerHTML = \"\";\n  var _loop = function _loop(i) {\n    var li = createItem(\"li\");\n    li.classList.add(\"todo__list-item\");\n    var input = createItem(\"input\");\n    input.setAttribute(\"type\", \"checkbox\");\n    input.classList.add(\"todo__list-item-input\");\n    var span = createItem(\"span\");\n    span.classList.add(\"todo__list-item-text\");\n    span.textContent = localArr[i];\n    input.addEventListener(\"change\", function () {\n      span.classList.toggle(\"done\");\n    });\n    var div = createItem(\"div\");\n    div.classList.add(\"todo__list-item-svg\");\n    var buttonEdit = createItem(\"button\");\n    buttonEdit.className = \"btn todo__list-item-edit\";\n    buttonEdit.addEventListener(\"click\", function () {\n      editTodoElement(i);\n    });\n    buttonEdit.removeEventListener(\"click\", function () {\n      editTodoElement(i);\n    });\n    var buttonDelete = createItem(\"button\");\n    buttonDelete.className = \"btn todo__list-item-delete\";\n    buttonDelete.addEventListener(\"click\", function () {\n      deleteTodoElement(i);\n    });\n    buttonDelete.removeEventListener(\"click\", function () {\n      deleteTodoElement(i);\n    });\n    div.append(buttonEdit, buttonDelete);\n    li.append(input, span, div);\n    todoList.append(li);\n  };\n  for (var i = 0; i < localArr.length; i++) {\n    _loop(i);\n  }\n}\nfunction addElement(e) {\n  e.preventDefault();\n  var value = todoInput.value;\n  if (value.length > 0) {\n    localArr.push(value);\n    todoInput.value = \"\";\n  }\n  setLocalStorage();\n  render();\n}\nfunction editTodoElement(index) {\n  var item = localArr[index];\n  if (index !== undefined) {\n    togglePopup();\n    popupInput.value = item;\n    popupSaveBtn.addEventListener(\"click\", function () {\n      localArr[index] = popupInput.value;\n      togglePopup();\n      setLocalStorage();\n      render();\n    });\n  }\n}\nfunction deleteTodoElement(index) {\n  if (index !== undefined) {\n    localArr.splice(index, 1);\n  }\n  localStorage.setItem(\"arr\", JSON.stringify(localArr));\n  render();\n}\ntodoAddBtn.addEventListener(\"click\", addElement);\nfunction togglePopup() {\n  popup.classList.toggle(\"open-popup\");\n}\npopupCancelBtn.addEventListener(\"click\", togglePopup);\npopupCloseBtn.addEventListener(\"click\", togglePopup);\nrender();\n\n//# sourceURL=webpack://todo-list-js/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/styles/index.css":
+/*!******************************!*\
+  !*** ./src/styles/index.css ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://todo-list-js/./src/styles/index.css?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
